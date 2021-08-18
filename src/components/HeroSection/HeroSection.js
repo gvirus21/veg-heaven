0809ad001f39971f0156738ff49e39e4384edc16
gourav <gvirus21yt@gmail.com>
2021-08-18 20:40:51 +0530

    stock avaibility is now showing and stock avaibility text color and  button background color change based on avaibility.

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ProductCard from "./ProductCard";
+import itemData from "../../data.json";
 
 const HeroSection = () => {
+  console.log(itemData);
+
   return (
     <div className="heroSection">
       <div className="heroSectionContainer">
         <h1 className="HeroHeading">Featured Products</h1>
         <div className="ProductContainer">
-          <ProductCard></ProductCard>
-          <ProductCard></ProductCard>
-          <ProductCard></ProductCard>
-          <ProductCard></ProductCard>
+          {itemData.map((item) => (
+            <ProductCard item={item} id={item.id} />
+          ))}
         </div>
       </div>
     </div>
--- src/components/HeroSection/ProductCard.js
+++ src/components/HeroSection/ProductCard.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import banana from "../../images/banana.jpg";
+import drumstick from "../../images/drumstick.jpg";
+import potato from "../../images/potato.jpg";
+import orange from "../../images/Oranges.jpg";
 
-const ProductCard = () => {
-  return <div className="productCard"></div>;
+const ProductCard = ({ item }) => {
+  const { name, id, price, available } = { ...item };
+  const [stock, setStock] = useState("Out of stock");
+
+  useEffect(() => {
+    if (available) {
+      setStock("In Stock");
+    } else {
+      setStock("Out of stock");
+    }
+  }, []);
+
+  const imgArr = [];
+  imgArr.push(potato, banana, drumstick, orange);
+
+  return (
+    <div className="productCard">
+      <img src={imgArr[id - 1]} alt={name} />
+      <h1>{name}</h1>
+      <p>price: ₹{price}</p>
+      {/* <h2 style={{ color: { available } && "green" }}>{stock}</h2> */}
+      <h2 style={{ color: available ? "green" : "red" }}>{stock}</h2>
+      <button style={{ opacity: available ? "100%" : "0" }}>Buy Now</button>
+    </div>
+  );
 };
 
 export default ProductCard;
